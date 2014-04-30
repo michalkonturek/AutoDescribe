a2a580a1cc7540aa0849f137e25d5ed1d31df162
Michal Konturek <michal.konturek@gmail.com>
2014-04-30 22:22:39 +0100

    Renamed project.

--- AutoDescribe/AutoDescribe/AppDelegate.h
+++ AutoDescribe/AutoDescribe/AppDelegate.h
@@ -1,9 +1,9 @@
 //
 //  AppDelegate.h
-//  MKDebugKit
+//  AutoDescribe
 //
-//  Created by Michal Konturek on 20/10/2013.
-//  Copyright (c) 2013 Michal Konturek. All rights reserved.
+//  Created by Michal Konturek on 30/04/2014.
+//  Copyright (c) 2014 Michal Konturek. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
